ae8240182531b862509aa837e43668039dfd213b
shani <dhar8870@mylaurier.ca>
2024-05-27 11:35:04 -0400

    Initial Commit

---
+++ Valid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Valid
+//
+//  Created by Shani Dharmasena on 2024-05-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Valid/ValidApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValidApp.swift
+//  Valid
+//
+//  Created by Shani Dharmasena on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct ValidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
